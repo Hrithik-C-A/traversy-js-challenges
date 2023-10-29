1e5ea451b89d2d3f0deb98bde43c4d030f289105
Hrithik C A <hrithikca@gmail.com>
2023-10-29 16:51:43 +0530

    solved

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,26 @@
-function validateEmail() {}
+function validateEmail(email) {
+
+    if (email.indexOf('@') === -1) {
+        return false;
+    }
+
+    const [localPart, domainPart] = email.split('@');
+    if ( localPart.length === 0 || domainPart.length < 3) {
+        return false;
+    }
+
+    const domainExtension = email.split('.');
+    if (domainExtension.length < 2 || domainExtension[domainExtension.length - 1].length < 2) {
+        return false;
+    } 
+
+    return true;
+
+
+    // const regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    
+    // return regex.test(email);
+
+}
 
 module.exports = validateEmail;
