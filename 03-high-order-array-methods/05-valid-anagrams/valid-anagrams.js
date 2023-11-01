5c5a2cffb73dee8fa4e50ea3584d301a0c104e85
Hrithik C A <hrithikca@gmail.com>
2023-11-01 15:16:25 +0530

    solved

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,13 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+    const string1 = str1.split('').sort().join('');
+    const string2 = str2.split('').sort().join('');
+    
+    if (string1 === string2) {
+      return true;
+    } else {
+      return false;
+    }
+  
+  }
 
 module.exports = validAnagrams;
--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,28 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+
+    // if (str.trim() === '' || str.trim().length > 140) {
+    //     return false
+    //   }
+
+    if (str.trim() === '') {
+      return false
+    }
+  
+    const words = str.trim().split(' ').map(item => {
+      const firstWord = item.slice(0,1).toUpperCase();
+      const remainingWord = item.slice(1);
+      
+      return `${firstWord}${remainingWord}`; 
+    });
+    
+    const hashtag = `#${words.join('')}`;
+    
+    if (hashtag.length > 140) {
+        return false
+    }
+
+    return hashtag;
+   
+  }
 
 module.exports = generateHashtag;
