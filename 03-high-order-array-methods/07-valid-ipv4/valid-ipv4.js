d0cec000c47bb9bd4a07d24f00589aa21cb223ca
Hrithik C A <hrithikca@gmail.com>
2023-11-02 17:47:42 +0530

    solved

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,18 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (input) => {
+    const inputArr = input.split('.');
+  
+    const condition1 = inputArr.length === 4;
+    const condition2 = inputArr.every(item => {
+        const num = parseInt(item);
+        return (num >= 0 && num <= 255) && item === num.toString();
+    });
+  
+    if (condition1 && condition2) {
+      return true;
+    } else {
+      return false;
+    }
+  
+  };
 
 module.exports = isValidIPv4;
--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,32 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    const numberOfCars = cars.length;
+    // const averageMileage = cars.reduce((average, car) => {
+    //   const result = ( average + car.mileage / numberOfCars );
+    //   return result;
+    // }, 0);
+    const totalMileage = cars.reduce((total, car) => total + car.mileage, 0);
+  
+    const averageMileage = totalMileage / numberOfCars;
+    
+    const mileage = cars.map(car => car.mileage).sort((a, b) => a - b);
+  
+    const carsWithHighestMileage = cars.find(car => {
+      const highestMileage = mileage[mileage.length -1];
+      return car.mileage === highestMileage;
+    });
+  
+    const carsWithLowestMileage = cars.find(car => {
+      const lowestMileage = mileage[0];
+      return car.mileage === lowestMileage;
+    });
+  
+      return {
+        averageMileage: averageMileage,
+        highestMileageCar: carsWithHighestMileage,
+        lowestMileageCar: carsWithLowestMileage,
+        totalMileage: totalMileage
+      }
+  
+  };
 
 module.exports = analyzeCarMileage;
