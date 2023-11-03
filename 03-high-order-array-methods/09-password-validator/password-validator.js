445a51df073637b7fa60d1d679ce655679feb652
Hrithik C A <hrithikca@gmail.com>
2023-11-03 16:01:37 +0530

    solved

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,26 @@
-function validatePassword() {}
+function validatePassword(password) {
+    
+    const hasRequiredLength = password.length >= 8;
+
+    const hasUpperCase = (pass) => {
+        // return password !== pass.toLowerCase();
+        return /[A-Z]/.test(pass);
+    }
+    const hasLowerCase = (pass) => {
+        // return password !== pass.toUpperCase();
+        return /[a-z]/.test(pass);
+    }
+
+    const hasDigits = (pass) => {
+        return /[0-9]/.test(pass);
+    }
+
+    if (hasRequiredLength && hasLowerCase(password) && hasUpperCase(password) && hasDigits(password)) {
+        return true;
+    } 
+
+    return false;
+
+}
 
 module.exports = validatePassword;
