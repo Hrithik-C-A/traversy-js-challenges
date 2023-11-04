b5a1454ffafa5ef91b8267ec8f4b611db9570cde
Hrithik C A <hrithikca@gmail.com>
2023-11-04 16:27:16 +0530

    solved

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    const unicodeArr = arr.map(item => item.charCodeAt());
+
+    const actualSum = unicodeArr.reduce((total, num) => total + num, 0);
+    const expectedSum = ((unicodeArr.length + 1)/2)*(unicodeArr[0] + unicodeArr[unicodeArr.length - 1]);
+    
+    const missingLetter = (expectedSum - actualSum);
+
+    return String.fromCharCode(missingLetter);
+}
 
 module.exports = findMissingLetter;
