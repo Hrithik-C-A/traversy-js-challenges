ef5438c7e35004f3575b60e93dd1cd521defe3cb
Hrithik C A <hrithikca@gmail.com>
2023-11-07 17:17:22 +0530

    solved

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+    if ( str === '') {
+        return '';
+    }
+    return reverseString(str.substring(1)) + str[0];
+
+    // return str === '' ? '' : reverseString(str.substring(1)) + str[0]
+}
 
 module.exports = reverseString;
