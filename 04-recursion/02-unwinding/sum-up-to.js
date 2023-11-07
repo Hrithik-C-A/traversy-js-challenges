69fd4cec58570f0433050d358ab2054cf111523a
Hrithik C A <hrithikca@gmail.com>
2023-11-07 16:53:23 +0530

    solved

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,13 @@
-function sumUpTo() {}
+function sumUpTo(n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    if ( n < 1) {
+        return 0
+    }
+
+    return n + sumUpTo(n - 1);
+}
 
 module.exports = sumUpTo;
