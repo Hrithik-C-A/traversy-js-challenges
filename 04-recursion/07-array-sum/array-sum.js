187d21ad0c8c110ab852dfec1815384188ca3041
Hrithik C A <hrithikca@gmail.com>
2023-11-11 17:25:35 +0530

    solved

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,12 @@
-function arraySum() {}
+function arraySum(arr) {
+
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    const newArr = arr.slice(1);
+
+    return arr[0] + arraySum(newArr);
+}
 
 module.exports = arraySum;
